9f33eddfb3ab8f57eb533f4e9a9f9c4449ac7c3e
sk0r Czybik <sk0r-Czybik@users.noreply.github.com>
2015-08-26 21:07:58 +0200

    Create includes.h

---
+++ Code/includes.h
@@ -0,0 +1,27 @@
+#ifndef _INCLUDES_H
+#define _INCLUDES_H
+
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <ShlObj.h>
+
+/*
+	TigerSnail CS:GO External Hack
+	For Counter-Strike: Global Offensive
+
+	Version: 0.1
+
+	Developed by sk0r / Czybik
+	Contact: Czybik_Stylez<0x40>gmx<0x2E>de
+
+	Read the file 'readme.txt' for more details
+
+	File: includes.h: General include files
+*/
+
+#endif
