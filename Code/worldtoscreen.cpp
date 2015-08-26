f7277536625797f049967fd04382d80354a1d3fd
sk0r Czybik <sk0r-Czybik@users.noreply.github.com>
2015-08-26 21:03:41 +0200

    Create worldtoscreen.cpp

---
+++ Code/worldtoscreen.cpp
@@ -0,0 +1,23 @@
+#include "wordtoscreen.h"
+#include "vars.h"
+
+/*
+	TigerSnail CS:GO External Hack
+	For Counter-Strike: Global Offensive
+
+	Version: 0.1
+
+	Developed by sk0r / Czybik
+	Contact: Czybik_Stylez<0x40>gmx<0x2E>de
+
+	Read the file 'readme.txt' for more details
+
+	File: worldtoscreen.cpp: WorldToScreen function implementations
+*/
+
+//======================================================================
+bool WorldToScreen(const w2smatrix_t& vMatrix, const Vector& vEntOrigin, Vector& vOut)
+{
+	return false;
+}
+//======================================================================
